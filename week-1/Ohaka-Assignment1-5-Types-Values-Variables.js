53b0ca343bdaea1358edfe05646e66d1137347af
Larry Ohaka <lohaka@my365.bellevue.edu>
2020-10-21 00:31:47 -0400

    new

---
+++ week-1/Ohaka-Assignment1-5-Types-Values-Variables.js
@@ -0,0 +1,20 @@
+//Chris' employee records//
+var firstName = "Chris"
+var lastName = "Johnson"
+var address = "2203 Ceaderbrok lane Jacksonville,FL"
+var payRate = "28.5"
+var hireRate = "1/10"
+
+//Kathy's employee records//
+var firstName = "Kathy"
+var lastName = "Griffin"
+var address = "304 Dire St  Severn,MD 20023"
+var payRate = "25.4"
+var hireRate = "1/8"
+
+//Jeff's employee records//
+var firstName = "Jeff"
+var lastName = "Stevenson"
+var address = "3000 Chestnut Bl San Diego,CA 90303"
+var payRate = "45.6"
+var hireRate = "1/4"
