6421d2209940de7f658d92871a4bcd1541c58c61
Larry Ohaka <lohaka@my365.bellevue.edu>
2020-10-21 00:07:30 -0400

    new

---
+++ week-1/Ohaka-Assignment1-4-PrimitiveTypes .js
@@ -0,0 +1,15 @@
+var firstName = "Larry";
+
+var lastName = "Ohaka";
+
+var age = "25";
+
+var street = "1220 N Highland St";
+
+var zipCode = "20021";
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(zipCode);
