c5ebad9f15c1bd2243e7251069087cfda4590d63
Larry Ohaka <lohaka@my365.bellevue.edu>
2020-10-28 22:10:05 -0400

    new

---
+++ week-2/Ohaka-Exercise-2.3-FunctionProperties.js
@@ -0,0 +1,19 @@
+/*
+============================================
+; Title:  Function Properties
+; Author: Larry Ohaka
+; Date:   28 October 2020
+; Description: Displays a formatted header
+;===========================================
+*/
+//function 
+function myName(){
+    return myName.larry;
+}
+
+// function properties
+myName.larry="Larry"
+
+
+//output
+console.log("Hi" + myName() + "Ohaka");
