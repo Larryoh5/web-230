6487aa2234a38cdb4621ccb77b0d3c047ad8a98e
Larry Ohaka <lohaka@my365.bellevue.edu>
2020-11-17 17:29:55 -0500

    new

---
+++ week-5/Ohaka-Exercise5.2-ES5builtinFunctions.js
@@ -0,0 +1,18 @@
+/* ============================================
+ ; Title: Arrays and Operators
+; Author: Professor Krasso
+; Date:   17 November 2020
+; Modified By: Larry Ohaka
+ ; Description: Keyed collection array with two errors
+ ;===========================================
+*/
+
+//link to my header
+const header = require('../Ohaka-header.js');
+//header
+console.log(header.display('Larry', 'Ohaka', 'Exercise 5.2') + '\n');
+
+var favFoods = ["Bulgogi", "Rice", "Fries", "Chicken"];
+favFoods.forEach(function(food){
+  console.log(food);
+});
