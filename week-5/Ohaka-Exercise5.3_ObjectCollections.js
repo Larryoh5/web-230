0fb2c450960ec6fe89ec973afea9157d76641c16
Larry Ohaka <lohaka@my365.bellevue.edu>
2020-11-17 20:25:45 -0500

    new

--- week-5/Ohaka-Exercise5.3_ObjectCollections.js
+++ week-5/Ohaka-Exercise5.3_ObjectCollections.js
@@ -7,38 +7,65 @@
  ;===========================================
 */
 
+/*
+  Expected output:
+  FirstName LastName
+  Exercise 5.3
+  Today's Date
+  -- COMPOSERS --
+  Last Name: Beethoven, Genre: Classical, Rating: 8
+  Last Name: Mozart, Genre: Classical, Rating: 10
+  Last Name: Bach, Genre: Classical, Rating: 9
+  Last Name: Haydn, Genre: Classical, Rating: 6
+  Last Name: Schubert, Genre: Classical, Rating: 5
+*/
+
+
 //link to my header
 const header = require('../Ohaka-header.js');
 //header
 console.log(header.display('Larry', 'Ohaka', 'Exercise 5.3') + '\n');
 
+
+console.log('-- DIRECTORS --');
+
 var directors = [
   {
     firstName: 'Wes',
-    lastName: 'Anderson'
+    lastName: 'Anderson',
+    genre:'Auteur',
+    rating: 9
   },
 
   {
     firstName: 'Bong',
-    lastName: 'Joon-ho'
+    lastName: 'Joon-ho',
+    genre:'Thriller',
+    rating: 8
   },
 
   {
     firstName: 'Steven',
-    lastName: 'Spielberg'
+    lastName: 'Spielberg',
+    genre:'Science Fiction',
+    rating: 9
   },
 
   {
     firstName: 'Quentin',
-    lastName: 'Tarantino'
+    lastName: 'Tarantino',
+    genre:'Auteur',
+    rating: 7
   },
 
   {
     firstName: 'David',
-    lastName: 'Fincher'
+    lastName: 'Fincher',
+    genre:'Neo Noir',
+    rating: 8
   },
 ]
 
 directors.forEach(function(films) {
-  console.log(films.firstName + " " + films.lastName)
+  console.log( "Last Name: " + films.lastName  + "," + " " + "Genre: " + films.genre  + ","  +  " " + "Rating: " + films.rating)
 });
